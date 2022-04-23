27d101c5d27ac83db9a2a0ed67f4f6ec45dcdc07
Evandro Pomatti <evandro.pomatti@gmail.com>
2022-04-23 11:29:47 -0300

    frontdoor bootstrap

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -55,49 +55,49 @@ output "kube_config" {
 
 ### Global Resources
 
-# resource "azurerm_resource_group" "global" {
-#   name     = "rg-${application_name}-global"
-#   location = var.main_location
-# }
+resource "azurerm_resource_group" "global" {
+  name     = "rg-${var.application_name}-global"
+  location = var.main_location
+}
 
-# resource "azurerm_frontdoor" "example" {
-#   name                = "ft-${application_name}"
-#   resource_group_name = azurerm_resource_group.frontdoor.name
+resource "azurerm_frontdoor" "global" {
+  name                = "ft-${var.application_name}"
+  resource_group_name = azurerm_resource_group.global.name
 
-#   routing_rule {
-#     name               = "exampleRoutingRule1"
-#     accepted_protocols = ["Http", "Https"]
-#     patterns_to_match  = ["/*"]
-#     frontend_endpoints = ["exampleFrontendEndpoint1"]
-#     forwarding_configuration {
-#       forwarding_protocol = "MatchRequest"
-#       backend_pool_name   = "exampleBackendBing"
-#     }
-#   }
+  routing_rule {
+    name               = "exampleRoutingRule1"
+    accepted_protocols = ["Http", "Https"]
+    patterns_to_match  = ["/*"]
+    frontend_endpoints = ["exampleFrontendEndpoint1"]
+    forwarding_configuration {
+      forwarding_protocol = "MatchRequest"
+      backend_pool_name   = "exampleBackendBing"
+    }
+  }
 
-#   backend_pool_load_balancing {
-#     name = "exampleLoadBalancingSettings1"
-#   }
+  backend_pool_load_balancing {
+    name = "exampleLoadBalancingSettings1"
+  }
 
-#   backend_pool_health_probe {
-#     name = "exampleHealthProbeSetting1"
-#   }
+  backend_pool_health_probe {
+    name = "exampleHealthProbeSetting1"
+  }
 
-#   backend_pool {
-#     name = "exampleBackendBing"
-#     backend {
-#       host_header = "www.bing.com"
-#       address     = "www.bing.com"
-#       http_port   = 80
-#       https_port  = 443
-#     }
+  backend_pool {
+    name = "exampleBackendBing"
+    backend {
+      host_header = "www.bing.com"
+      address     = "www.bing.com"
+      http_port   = 80
+      https_port  = 443
+    }
 
-#     load_balancing_name = "exampleLoadBalancingSettings1"
-#     health_probe_name   = "exampleHealthProbeSetting1"
-#   }
+    load_balancing_name = "exampleLoadBalancingSettings1"
+    health_probe_name   = "exampleHealthProbeSetting1"
+  }
 
-#   frontend_endpoint {
-#     name      = application_name
-#     host_name = "${application_name}.azurefd.net"
-#   }
-# }
+  frontend_endpoint {
+    name      = "exampleFrontendEndpoint1"
+    host_name = "ft-${var.application_name}.azurefd.net"
+  }
+}
