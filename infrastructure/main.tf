8bb294d3621183621fa5cd2b85ef2331515525d8
Evandro Pomatti <evandro.pomatti@gmail.com>
2022-04-23 11:41:56 -0300

    frontdoor nginx

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -65,39 +65,39 @@ resource "azurerm_frontdoor" "global" {
   resource_group_name = azurerm_resource_group.global.name
 
   routing_rule {
-    name               = "exampleRoutingRule1"
+    name               = "nginxRoutingRule"
     accepted_protocols = ["Http", "Https"]
     patterns_to_match  = ["/*"]
-    frontend_endpoints = ["exampleFrontendEndpoint1"]
+    frontend_endpoints = ["nginxFrontend"]
     forwarding_configuration {
       forwarding_protocol = "MatchRequest"
-      backend_pool_name   = "exampleBackendBing"
+      backend_pool_name   = "nginxBackendPool"
     }
   }
 
   backend_pool_load_balancing {
-    name = "exampleLoadBalancingSettings1"
+    name = "nginxLoadBalancing"
   }
 
   backend_pool_health_probe {
-    name = "exampleHealthProbeSetting1"
+    name = "nginxHealthProbe"
   }
 
   backend_pool {
-    name = "exampleBackendBing"
+    name = "nginxBackendPool"
     backend {
-      host_header = "www.bing.com"
-      address     = "www.bing.com"
+      host_header = "20.201.0.181"
+      address     = "20.201.0.181"
       http_port   = 80
       https_port  = 443
     }
 
-    load_balancing_name = "exampleLoadBalancingSettings1"
-    health_probe_name   = "exampleHealthProbeSetting1"
+    load_balancing_name = "nginxLoadBalancing"
+    health_probe_name   = "nginxHealthProbe"
   }
 
   frontend_endpoint {
-    name      = "exampleFrontendEndpoint1"
+    name      = "nginxFrontend"
     host_name = "ft-${var.application_name}.azurefd.net"
   }
 }
