e685452d1bbc6363c99f6e890f189c24d39b4204
Evandro Pomatti <evandro.pomatti@gmail.com>
2022-05-03 11:18:10 -0300

    fix url

--- microservices/poll/src/mongo.py
+++ microservices/poll/src/mongo.py
@@ -5,11 +5,9 @@ from . import utils
 
 
 def get_collection(database: str, collection: str):
-    print("getting collection")
     use_keyvault = utils.to_bool(os.environ['USE_KEYVAULT'])
     if use_keyvault is True:
         connection_string = keyvault.get_cosmos_connection_string()
     else:
         connection_string = os.environ['COSMOSDB_CONNECTION_STRING']
-    print(connection_string)
     return MongoClient(connection_string).get_database(database).get_collection(collection)
