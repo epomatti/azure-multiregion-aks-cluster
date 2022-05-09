f94fc90ea05785985feebb693cb93f3ee036dadb
Evandro Pomatti <evandro.pomatti@gmail.com>
2022-05-09 20:53:15 -0300

    bump k8s

--- infrastructure/azure/scripts/tools.sh
+++ infrastructure/azure/scripts/tools.sh
@@ -12,3 +12,11 @@ sudo apt-get update && sudo apt-get install terraform -y
 
 # Azure CLI
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+# Kubectl
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
